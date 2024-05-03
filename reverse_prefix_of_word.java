f94099bad02b7870691fb7543ee0e97d140220c2
Zeel Shah <145953352+zeelshah1805@users.noreply.github.com>
2024-05-03 18:58:35 +0530

    Create reverse_prefix_of_word.java

---
+++ reverse_prefix_of_word.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if (index == -1) return word;
+        
+        StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
+        return sb.reverse().toString() + word.substring(index + 1);
+    }
+}
